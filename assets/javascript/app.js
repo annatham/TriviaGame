b66ebc55d4155c4bd584b4f626139981f584071f
Anna Thammakane <anna.thammakane@gmail.com>
2019-10-03 17:53:07 -0500

    added formatting to html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,9 @@
 console.log("let's play a game!");
+
+// create countdown timer
+
+// only allow one answer to be selected
+
+// on click event to load results
+
+// create function to calculate results
